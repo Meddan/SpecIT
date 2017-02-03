8745003730ffda046d486c5659de2d97704fcc9b
Meddan <christoffer.medin@gmail.com>
2017-02-03 13:36:46 +0100

    We now handle assertions at the beggining of functions

--- src/main/java/ContractGenerator.java
+++ src/main/java/ContractGenerator.java
@@ -1,5 +1,6 @@
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
@@ -7,8 +8,8 @@ import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.ConditionalExpr;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.stmt.ReturnStmt;
-import com.github.javaparser.ast.stmt.ThrowStmt;
+import com.github.javaparser.ast.stmt.*;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.google.common.base.Strings;
 
 import java.io.File;
@@ -38,12 +39,55 @@ public class ContractGenerator {
                 contracts.put((MethodDeclaration) b, createContract((MethodDeclaration) b));
             }
         }
+        for(MethodDeclaration md : contracts.keySet()){
+            System.out.println("-----------------");
+            System.out.println();
+            System.out.println(md.getName() + " " + contracts.get(md));
+            System.out.println();
+            System.out.println("-----------------");
+        }
 
 
     }
     public String createContract(MethodDeclaration md){
+        //Get row for md
+        //Get top-level assertions
+        NodeList<Statement> stmtList =  md.getBody().get().getStatements();
+        for(Statement s : stmtList){
+            if(s instanceof AssertStmt){
+                System.out.println("found assert");
+                //Check if first / statements can be ignored
+
+                if(stmtList.get(0) == s){
+                    return "requires " +  ((AssertStmt) s).getCheck().toString();
+                }
+                //Check distance from start / end to determine if assertion is pre/post condition
+                //Check if statements between allow for ignore
+            }
+        }
+
         return null;
-
     }
-
+    public static void main(String args[]){
+        File projectDir = new File("src/main/java/Examples");
+        testClasses(projectDir);
+    }
+    public static void testClasses(File projectDir) {
+        new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
+            System.out.println(path);
+            System.out.println(Strings.repeat("=", path.length()));
+            try {
+                new VoidVisitorAdapter<Object>() {
+                    @Override
+                    public void visit(ClassOrInterfaceDeclaration n, Object arg) {
+                        super.visit(n, arg);
+                        ContractGenerator cg = new ContractGenerator(n);
+                    }
+                }.visit(JavaParser.parse(file), null);
+                System.out.println(); // empty line
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).explore(projectDir);
+    }
 }
