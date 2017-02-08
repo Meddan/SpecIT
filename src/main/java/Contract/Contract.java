73bb1047cc4094c1961798f728beadf2e9ffb503
Pontus Doverstav <doverstav@gmail.com>
2017-02-08 14:21:56 +0100

    Added basic methods for contract

--- src/main/java/Contract/Contract.java
+++ src/main/java/Contract/Contract.java
@@ -1,5 +1,7 @@
 package Contract;
 
+import java.util.LinkedList;
+
 /**
  * Created by dover on 2017-02-08.
  */
@@ -9,4 +11,40 @@ package Contract;
     a complete contract.
  */
 public class Contract {
+
+    LinkedList<Behavior> behaviors = new LinkedList<Behavior>();
+
+    Behavior currentBehavior;
+
+    /**
+     * Add a new behavior to this contract and sets the current behavior to
+     * be the newly created one.
+     *
+     * @param isExceptional Flag indicating whether behavior is exceptional or not
+     */
+    public void addBehavior(boolean isExceptional){
+        currentBehavior = new Behavior(isExceptional);
+        behaviors.add(currentBehavior);
+    }
+
+    public void addPreCon(String preCon){
+        currentBehavior.addPreCon(preCon);
+    }
+
+    public void addPostCon(String postCon){
+        currentBehavior.addPostCon(postCon);
+    }
+
+    public void addAssignable(String assignable){
+        currentBehavior.addAssignable(assignable);
+    }
+
+    public void addException(String exception){
+        currentBehavior.addException(exception);
+    }
+
+    public LinkedList<Behavior> getBehaviors(){
+        return behaviors;
+    }
+
 }
