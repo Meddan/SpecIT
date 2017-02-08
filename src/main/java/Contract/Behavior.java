c577554b27dec563fece4e16a5c32a2717674131
Pontus Doverstav <doverstav@gmail.com>
2017-02-08 11:19:23 +0100

    Created base for own contract-class

---
+++ src/main/java/Contract/Behavior.java
@@ -0,0 +1,14 @@
+package Contract;
+
+/**
+ * Created by dover on 2017-02-08.
+ */
+
+/*
+    A class for modeling the various behaviours that can be seen in a contract, i.e.
+    normal and exceptional behavior.
+ */
+public class Behavior {
+    
+
+}
---
+++ src/main/java/Contract/Contract.java
@@ -0,0 +1,12 @@
+package Contract;
+
+/**
+ * Created by dover on 2017-02-08.
+ */
+
+/*
+    A class for modelling contracts. Contains several behaviors, which together will create
+    a complete contract.
+ */
+public class Contract {
+}
