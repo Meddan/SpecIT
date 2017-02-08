54aecdb44cbead933a1094a7459b1dd7488b644e
Pontus Doverstav <doverstav@gmail.com>
2017-02-08 12:26:32 +0100

    toString supports assignable, signals, requires, ensures

--- src/main/java/Contract/Behavior.java
+++ src/main/java/Contract/Behavior.java
@@ -17,13 +17,16 @@ public class Behavior {
 
     // List for representing preconditions. The pre-condition "requires !n"
     // should be represented in this list as "!n"
-    LinkedList<String> preCons = new LinkedList<String>();
+    private LinkedList<String> preCons = new LinkedList<String>();
 
     // List for represeting postconditions. See above for format.
-    LinkedList<String> postCons = new LinkedList<String>();
+    private LinkedList<String> postCons = new LinkedList<String>();
 
     // List for representing any assignable variable
-    LinkedList<String> assignables = new LinkedList<String>();
+    private LinkedList<String> assignables = new LinkedList<String>();
+
+    // List for representing exceptions thrown when behavior is exceptional
+    private LinkedList<String> exceptions = new LinkedList<String>();
 
     /**
      * Constructs a new instance of a behavior.
@@ -46,6 +49,15 @@ public class Behavior {
         assignables.add(assignable);
     }
 
+    public void addException(String exception){
+        if (isExceptional) {
+            exceptions.add(exception);
+        } else {
+            throw new Error("Behavior is not exceptional");
+        }
+
+    }
+
     public LinkedList<String> getPreCons(){
         return preCons;
     }
@@ -58,8 +70,122 @@ public class Behavior {
         return assignables;
     }
 
+    public LinkedList<String> getExceptions(){
+        return exceptions;
+    }
+
     public boolean getIsExceptional(){
         return isExceptional;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(createBehaviorHeader());
+        sb.append(createPreCons());
+        sb.append(createPostCons());
+        sb.append(createSignalsOnly());
+        sb.append(createSignal());
+        sb.append(createAssignable());
+
+        return sb.toString();
+    }
+
+    private String createPreCons(){
+        StringBuilder sb = new StringBuilder();
+        // Write out preconditions
+        for(String s : preCons){
+            sb.append("requires ");
+            sb.append(s);
+            sb.append(";\n");
+        }
+
+        return sb.toString();
+    }
+
+    private String createPostCons(){
+        StringBuilder sb = new StringBuilder();
+
+        // Write out postconditions
+        for(String s : postCons){
+            sb.append("ensures ");
+            sb.append(s);
+            sb.append(";\n");
+        }
+
+        return sb.toString();
+    }
+
+    private String createAssignable(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("assignable ");
+        for(String s : assignables){
+            sb.append(s);
+            if(assignables.getLast().equals(s)){
+                // If we're at the last element
+                sb.append(";\n");
+            } else {
+                // If we're not at the last element
+                sb.append(", ");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private String createBehaviorHeader(){
+        if(isExceptional){
+            return "public exceptional_behavior\n";
+        } else {
+            return "public normal_behavior\n";
+        }
+    }
+
+    private String createSignalsOnly(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("signals_only ");
+
+        for(String s : exceptions) {
+            sb.append(s);
+            if (exceptions.getLast().equals(s)) {
+                // If we're at the last element
+                sb.append(";\n");
+            } else {
+                // If we're not at the last element
+                sb.append(", ");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private String createSignal(){
+        StringBuilder sb = new StringBuilder();
+
+        for(String s : exceptions){
+            sb.append("signal ");
+            sb.append(s);
+            sb.append(" (");
+            sb.append(concatPreCons());
+            sb.append(");\n");
+        }
+
+        return sb.toString();
+    }
+
+    private String concatPreCons(){
+        StringBuilder sb = new StringBuilder();
+
+        for(String s : preCons){
+            sb.append(s);
+            if(!preCons.getLast().equals(s)){
+                sb.append(" && ");
+            }
+        }
+
+        return sb.toString();
     }
 
 }
