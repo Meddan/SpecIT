6108c2e10137506cd5e085af40edd98c7c75e6ce
Pontus Doverstav <doverstav@gmail.com>
2017-02-08 11:28:26 +0100

    Added basic methods for behavior class

--- src/main/java/Contract/Behavior.java
+++ src/main/java/Contract/Behavior.java
@@ -1,5 +1,7 @@
 package Contract;
 
+import java.util.LinkedList;
+
 /**
  * Created by dover on 2017-02-08.
  */
@@ -9,6 +11,39 @@ package Contract;
     normal and exceptional behavior.
  */
 public class Behavior {
-    
+
+    // Flag for showing whether behavior is exceptional or not
+    boolean isExceptional;
+
+    // List for representing preconditions. The pre-condition "requires !n"
+    // should be represented in this list as "!n"
+    LinkedList<String> preCons = new LinkedList<String>();
+
+    // List for represeting postconditions. See above for format.
+    LinkedList<String> postCons = new LinkedList<String>();
+
+    // List for representing any assignable variable
+    LinkedList<String> assignables = new LinkedList<String>();
+
+    /**
+     * Constructs a new instance of a behavior.
+     *
+     * @param isExceptional Flag indicating whether behavior is exceptional or not
+     */
+    public Behavior (boolean isExceptional){
+        this.isExceptional = isExceptional;
+    }
+
+    public void addPreCon(String preCon){
+        preCons.add(preCon);
+    }
+
+    public void addPostCon(String postCon){
+        postCons.add(postCon);
+    }
+
+    public void addAssignable(String assignable){
+        assignables.add(assignable);
+    }
 
 }
