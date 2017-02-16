12acff30dac5e2acb25983a279a2decaca55abd7
Pontus Doverstav <doverstav@gmail.com>
2017-02-16 11:56:42 +0100

    ExceptionCondition is now built around a list of expressions instead

--- src/main/java/Contract/ExceptionCondition.java
+++ src/main/java/Contract/ExceptionCondition.java
@@ -1,22 +1,32 @@
 package Contract;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.type.Type;
+
+import java.util.LinkedList;
+
 /**
  * Created by meddan on 10/02/17.
  */
 public class ExceptionCondition {
     private Type type;
-    private Expression expression;
+    /* List of expressions to hold after axception is thrown */
+    private LinkedList<Expression> expressions = new LinkedList<Expression>();
+
     public ExceptionCondition(Type t, Expression e){
         type = t;
-        expression = e;
+        expressions.add(e);
+    }
+
+    public ExceptionCondition(Type t, LinkedList<Expression> e){
+        type = t;
+        expressions = e;
     }
 
     public Type getType() {
         return type;
     }
 
-    public Expression getName() {
-        return expression;
+    public LinkedList<Expression> getName() {
+        return expressions;
     }
 }
