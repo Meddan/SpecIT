136915edddc0f6f03185b5c7b8ae4ebd05778dcc
Pontus Doverstav <doverstav@gmail.com>
2017-03-02 10:50:26 +0100

    Expressions in ensures are now properly enclosed

--- src/main/java/Contract/PostCondition.java
+++ src/main/java/Contract/PostCondition.java
@@ -26,7 +26,7 @@ public class PostCondition {
             return "";
         }
         if(isReturn){
-            return "ensures \\result == " + expression.toString() + ";\n";
+            return "ensures \\result == (" + expression.toString() + ");\n";
         } else {
             return "ensures " + expression.toString() + ";\n";
         }
