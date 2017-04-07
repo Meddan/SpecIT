fbe486f78762d44eb7ec8c97e996949d31dca6e6
Meddan <christoffer.medin@gmail.com>
2017-04-07 14:50:53 +0200

    New class to represent a variable in our contracts which was previosly done using SimpleName. The variables name is without modifiers (this. Class.) and that information is saved in the enum Scope. The Variable also keeps track of what class it belongs to.

---
+++ src/main/java/Contract/Variable.java
@@ -0,0 +1,34 @@
+package Contract;
+
+public class Variable {
+    public String getName() {
+        return name;
+    }
+    public enum Scope {
+        local, field, staticfield;
+    }
+    public String getClassName() {
+        return className;
+    }
+    private Scope scope;
+    private String name;
+    private String className;
+    private Scope getScope(){
+        return this.scope;
+    }
+    public Variable(Scope s, String name, String clazz){
+        this.scope = s;
+        this.className = clazz;
+        this.name = name;
+    }
+    public boolean equals(Object o){
+        if(o == null){
+            return false;
+        }
+        if(o instanceof Variable) {
+            Variable v = (Variable) o;
+            return (v.getScope() == this.scope && className.equals(v.getClassName()) && name.equals(v.getName()));
+        }
+        return false;
+    }
+}
