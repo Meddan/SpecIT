10ce9f7bc95dd00518cbc77afd96a5b8223dc323
Meddan <christoffer.medin@gmail.com>
2017-04-21 15:55:56 +0200

    Added missing file

---
+++ src/main/java/ContractGeneration/UnresolvedParameterException.java
@@ -0,0 +1,8 @@
+package ContractGeneration;
+
+/**
+ * Exception representing inability to resolve the expression in a methodcall.
+ */
+public class UnresolvedParameterException extends Exception {
+
+}
