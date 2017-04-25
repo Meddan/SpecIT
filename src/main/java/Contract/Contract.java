67d15341ec06de4124c284a8fb7668801677fa16
Pontus Doverstav <doverstav@gmail.com>
2017-04-25 11:58:51 +0200

    Failing comment is now actually a comment so our JML is valid

--- src/main/java/Contract/Contract.java
+++ src/main/java/Contract/Contract.java
@@ -74,7 +74,7 @@ public class Contract {
         for (Behavior b : getLeafs(initialBehavior)) {
             if (!b.isEmpty()) {
                 if(b.getFailing().isPresent()){
-                    sb.append("Failing behavior : " + b.getFailing().get().getClass() + "\n");
+                    sb.append("// Failing behavior : " + b.getFailing().get().getClass() + "\n");
                 }
                 sb.append(b.toString());
                 sb.append("\nalso\n\n");
